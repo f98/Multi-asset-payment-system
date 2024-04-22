df7a435cc4df4f051896ddc85d632fadf43d3dde
f98 <86986438+f98@users.noreply.github.com>
2024-04-22 02:51:42 +0100

    Add files via upload

---
+++ pages/shares.py
@@ -0,0 +1,12 @@
+
+class shares:
+    def _init_(self,name,acquisation_price,number_of_shares):
+     self.name=name  
+     self.acquisation_price=acquisation_price 
+     self.number_of_shares= number_of_shares
+    
+
+    
+    
+
+
