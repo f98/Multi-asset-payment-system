31aa2b3b0377f94fd22fdc90f30bdffea1e073d4
f98 <86986438+f98@users.noreply.github.com>
2024-04-30 12:30:07 +0100

    Update join me as a cofounder.py

--- join me as a cofounder.py
+++ join me as a cofounder.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.title('Join me as a co-founder and become part of the next leap in payment industry')
 
 # Add the Google Form link
 st.markdown("[Kindly fill in this google form ](https://docs.google.com/forms/d/e/1FAIpQLSeQOgiPsBa6id3PBmKe4uU8whnckPzaOXnmoX_jYyBK6DUEkw/viewform?usp=sf_link)")
