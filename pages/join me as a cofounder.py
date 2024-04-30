ecfd664c20aa2c9b87a422fe521e07437e224641
f98 <86986438+f98@users.noreply.github.com>
2024-04-30 12:27:23 +0100

    Add files via upload

---
+++ pages/join me as a cofounder.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+# Add the Google Form link
+st.markdown("[Kindly fill in this google form ](https://docs.google.com/forms/d/e/1FAIpQLSeQOgiPsBa6id3PBmKe4uU8whnckPzaOXnmoX_jYyBK6DUEkw/viewform?usp=sf_link)")
